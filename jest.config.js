b4101693a4918796ea79ef2bbd3f219cd4af0276
V-Kozintsev <v_kozintcev@mail.ru>
2025-02-06 13:19:20 +0300

    I moved the mock folder and added the settings of the module to jest

--- jest.config.js
+++ jest.config.js
@@ -91,7 +91,7 @@ const config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^ymaps3$": "<rootDir>/__mocks__/ymaps3.js",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
