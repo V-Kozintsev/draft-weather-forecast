c6fb33314434ffeb8ae2d44d2fc51974acb47e42
V-Kozintsev <v_kozintcev@mail.ru>
2025-02-06 14:22:06 +0300

    final test check

--- src/tests/Map.test.jsx
+++ src/tests/Map.test.jsx
@@ -1,10 +1,18 @@
-// Map.test.jsx
+// src/tests/Map.test.jsx
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import { Map } from "../components";
 
 describe("Map Component", () => {
   afterEach(cleanup);
+
+  beforeEach(() => {
+    jest.spyOn(globalThis.console, "warn").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
   it("renders without crashing", () => {
     const { container } = render(<Map longitude={30} latitude={50} />);
